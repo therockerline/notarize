05aedb23aee2e981c2de54b566b72f5198b05715
therockerline <therockerline@gmail.com>
2024-04-19 11:26:26 +0200

    update log

--- src/notarytool.ts
+++ src/notarytool.ts
@@ -83,8 +83,8 @@ export async function notarizeAndWaitForNotaryTool(opts: NotaryToolStartOptions)
     ];
 
     const result = await spawn('xcrun', notarizeArgs);
+    d(result, notarizeArgs)
     const parsed = JSON.parse(result.output.trim());
-    d(parsed, notarizeArgs)
 
     if (result.code !== 0 || !parsed.status || parsed.status !== 'Accepted') {
       try {
